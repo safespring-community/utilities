640bccb15a4ff1b849caa6ede106423c3d3555c4
Jarle Bjørgeengen <jarle@safespring.com>
2022-01-07 16:19:45 +0100

    CEM-81 Work in progress. EOW

---
+++ minio-server/main.tf
@@ -0,0 +1,49 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+module sg {
+   source = "github.com/safespring-community/terraform-modules/v2-compute-security-group"
+   name = "mino_external_access"
+   description = "External access for mino"
+   rules = [
+     {
+       ip_protocol = "tcp"
+       to_port = "22"
+       from_port = "22"
+       cidr = "0.0.0.0/0"
+     },
+     {
+       ip_protocol = "tcp"
+       to_port = "7443"
+       from_port = "7443"
+       cidr = "0.0.0.0/0"
+     },
+     {
+       ip_protocol = "icmp"
+       to_port = "-1"
+       from_port = "-1"
+       cidr = "0.0.0.0/0"
+     }
+  ]
+}
+
+resource "openstack_compute_keypair_v2" "skp" {
+  name       = "mino-kp"
+  public_key = "${chomp(file("id_rsa.pub"))}"
+}
+
+module  minio_server{
+  source = "github.com/safespring-community/terraform-modules/v2-compute-local-disk"
+  key_pair_name = openstack_compute_keypair_v2.skp.name
+  security_groups = [module.sg.name]
+  prefix = "minio"
+  domain_name = "safedc.net"
+  role = "minio_server"
+}
+
