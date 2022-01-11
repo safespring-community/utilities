8aca2d3b900f9910d65c4f88d38d13d8414fc26a
Jarle Bjørgeengen <jarle@safespring.com>
2022-01-11 17:15:38 +0100

    CEM-81 A working minio server with mounted data disk. Manual certs.

--- minio-server/main.tf
+++ minio-server/main.tf
@@ -20,8 +20,14 @@ module sg {
      },
      {
        ip_protocol = "tcp"
-       to_port = "7443"
-       from_port = "7443"
+       to_port = "80"
+       from_port = "80"
+       cidr = "0.0.0.0/0"
+     },
+     {
+       ip_protocol = "tcp"
+       to_port = "9091"
+       from_port = "9091"
        cidr = "0.0.0.0/0"
      },
      {
@@ -35,15 +41,18 @@ module sg {
 
 resource "openstack_compute_keypair_v2" "skp" {
   name       = "mino-kp"
+  # Change this to where the pubkey is located
   public_key = "${chomp(file("id_rsa.pub"))}"
 }
 
 module  minio_server{
-  source = "github.com/safespring-community/terraform-modules/v2-compute-local-disk"
+  source = "github.com/safespring-community/terraform-modules/v2-compute-local-disk-and-attached-disk"
   key_pair_name = openstack_compute_keypair_v2.skp.name
   security_groups = [module.sg.name]
   prefix = "minio"
+  volume_size = 20
   domain_name = "safedc.net"
+  flavor = "lb.small"
   role = "minio_server"
 }
 
