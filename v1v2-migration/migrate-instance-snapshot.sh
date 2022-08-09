ce0bded4d6b6c861ef760a48ede794d5bb547b33
Gabriel Paues <gabriel.paues@safespring.com>
2022-08-09 14:30:13 +0200

    Update to reflect clouds.yaml config scheme

--- v1v2-migration/migrate-instance-snapshot.sh
+++ v1v2-migration/migrate-instance-snapshot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ $# -ne 2 ]
   then
-     echo "USAGE: migrate-boot-vol.sh [rc-file source] [rc-file-destination]"
-    echo "You need two OpenStack rc-files for API connection, one for source and one for the destination platform."
+     echo "USAGE: migrate-instance-snapshot.sh [os-cloud source] [os-cloude destination]"
+    echo "You need API access correctly setup with a clouds.yaml. Please go to https://docs.openstack.org/python-openstackclient/pike/configuration/index.html to get directions for how to set that up."
     echo "The script migrates a snapshot from the source platform to the destination platform."
     echo "When running the script you will be provided with a list of images/snapshots and asked to type which snapshot to migrate to the new platform"
     echo "For a secure migration the instance should be shut down before taking the snapshot"
@@ -10,21 +10,16 @@ if [ $# -ne 2 ]
     exit 1
 fi
 
-# Cleaning up OS-variables before reading source platform variables
-for x in `env|grep OS|cut -d= -f 1`; do unset $x; done
-# Read source platform variables
-source $1
-
-if ! openstack --insecure image list; then
+if ! openstack --insecure --os-cloud=$1 image list; then
     echo $(date -u) ":No contact with source platform"
     exit 1
 fi
 
 echo $(date -u) ":Please provide image name:"
 read imgname
-MIN_DISK=`openstack --insecure image show -f json $imgname|jq -r '.min_disk'`
+MIN_DISK=`openstack --insecure --os-cloud=$1 image show -f json $imgname|jq -r '.min_disk'`
 echo $(date -u) ":Downloading image $imgname with min_disk=$MIN_DISK"
-openstack --insecure image save "$imgname" > "$imgname".raw
+openstack --insecure --os-cloud=$1 image save "$imgname" > "$imgname".raw
 echo $(date -u) ":Converting image to $imgname.qcow2"
 qemu-img convert -f raw -O qcow2 "$imgname".raw "$imgname".qcow2
 if [ $? -eq 0 ] 
@@ -37,12 +32,11 @@ then
 fi
 
 # Cleaning up OS-variables before reading target platform variables
-for x in `env|grep OS|cut -d= -f 1`; do unset $x; done
+# for x in `env|grep OS|cut -d= -f 1`; do unset $x; done
 # Reading target platform variables
-source $2
 
 echo $(date -u) ":Uploading $imgname.qcow2 to destination platform"
-if ! openstack image create --disk-format qcow2 --container-format bare --private --min-disk "$MIN_DISK" "$imgname" < "$imgname".qcow2; then
+if ! openstack --os-cloud=$2 image create --disk-format qcow2 --container-format bare --private --min-disk "$MIN_DISK" "$imgname" < "$imgname".qcow2; then
     echo $(date -u) ":No contact with destination platform"
     exit 1
 fi
